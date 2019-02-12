47107779f97e4b92e531582e2fc24202437570c4
David Goldstein <david.goldstein@deciphernow.com>
2019-02-11 23:13:18 -0500

    code is working

--- random-wiki.go
+++ random-wiki.go
@@ -6,40 +6,47 @@ import (
     "net/http"
     "log"
     "github.com/prometheus/client_golang/prometheus/promhttp"
-    "github.com/davecgh/go-spew/spew"
+    // "github.com/davecgh/go-spew/spew"
     "io/ioutil"
 )
 
 type RArticleResp struct {
-    Query       RQuery      `json:"query"`
+    Query RQuery        `json:"query"`
 }
 
 type RQuery struct {
-    Pages       map[string]interface{}      `json:"pages"`
+    Pages  map[string]Page     `json:"pages"`
+}
+
+type Page struct {
+    Extract interface{} `json:"extract"`
 }
 
 
 func randomArticle(w http.ResponseWriter, r *http.Request) {
-
-    url := "https://www.mediawiki.org/w/api.php?action=query&generator=random&grnnamespace=0&grnlimit=1&prop=info&format=json"
+    // make request
+    url := "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exlimit=max&explaintext&exintro&generator=random&grnnamespace=0&grnlimit=1ts="
     res, err := http.Get(url)
     if err != nil {
         panic(err.Error())
     }
-
     body, err := ioutil.ReadAll(res.Body)
     if err != nil {
         panic(err.Error())
     }
-
     rArticle := &RArticleResp{} // or &Foo{}
     err = json.Unmarshal(body, &rArticle)
     if(err != nil){
         fmt.Println("whoops:", err)
     }
-    spew.Dump(rArticle)
-
-    fmt.Fprintf(w, "temp")
+    
+    text := ""
+    for _, v := range rArticle.Query.Pages {
+        text = fmt.Sprint(v.Extract)
+        break
+    }
+    
+    fmt.Fprintf(w, text)
 }
 
 func main() {
